ce82f16204f7466272d165d45943d0591aa8facd
Anand Kumar <anandkumar.s@commerceiq.ai>
2020-06-20 16:36:48 +0530

    My button initialised

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <my-button text="My Library Button"></my-button>
+  </div>
+</template>
+
+<script>
+import MyButton from './components/button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button" @click="buttonClicked">{{text}}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        text:{
+            type: String, 
+            default: 'default button action'
+        }
+    },
+    methods:{
+        buttonClicked(event){
+            this.$emit('my-button-click',event)
+        }
+    }
+
+}
+</script>
+
+<style>
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
